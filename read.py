c2a66af984309691649f2a702e87a4f28911f704
Nick Lin <a778977@gmail.com>
2019-08-27 20:27:08 +0800

    add filter function

--- read.py
+++ read.py
@@ -13,3 +13,12 @@ sum_len = 0
 for d in data:
     sum_len += len(d)
 print('平均是長度是', sum_len/len(data))
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有',len(new),'留言長度小魚100')
+print(new[0])
+
+
